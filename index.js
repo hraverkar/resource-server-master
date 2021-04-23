5f785d66726df00c37f3f64fb3bdacc7529dafbf
Harshal Raverkar <harshal.raverkar@gmail.com>
2021-04-23 10:15:41 +0530

    res.header addded

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const Sequelize = require('sequelize');
 const cors = require('cors');
 var Config = require('./config');
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");
+    next();
+});
+
 app.use(express.urlencoded({
   extended: true,
   limit: "5mb"
